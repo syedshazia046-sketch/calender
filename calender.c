ea6636d8bc69b7bcbf8b6bb16ce9eb2052eb42b7
syedshazia046-sketch <syedshazia046@gmail.com>
2025-12-11 18:27:05 +0530

    Update calender.c

--- calender.c
+++ calender.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-
+//give the first day of the year
 int getfirstdayofyear(int year)
 {
     int day;
     day = (year + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400) % 7;
     return day;
 }
-
+//checks whether the year is leap year or not
 int leapyear(int year)
 {
     if ((year%4 == 0 && year % 100 != 0) || year % 400 == 0)
@@ -18,21 +18,22 @@ int leapyear(int year)
         return 0;
     }
 }
-
+//this function is used for printing the months in the calender
 void printmonth(int year)
 {
     char *month[] = {"January", "February", "March", "April", "May", "June",
                      "July", "August", "September", "October", "November", "December"};
-    
+    //array of months
     int daysinamonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
+    //array of days of the month
     if (leapyear(year))
     {
-        daysinamonth[1] = 29;
+        daysinamonth[1] = 29;//if it is leap year then the days will be 29 in february
     }
 
     int startday = getfirstdayofyear(year);
-    FILE *calender;
+    FILE *calender;//creating a file pointer
+    //opening file calender.txt
     calender=fopen("calender.txt","a");
     if (calender == NULL)
 {
@@ -40,17 +41,17 @@ void printmonth(int year)
     return; // Exit the function if the file couldn't be opened
 }
 
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < 12; i++)//print the months
     {
         fprintf(calender,"\n\n-------------%s %d-------------\n", month[i], year);
         fprintf(calender,"Sun Mon Tue Wed Thu Fri Sat\n");
 
-        for (int k = 0; k < startday; k++)
+        for (int k = 0; k < startday; k++)//print the space in the calender
         {
             fprintf(calender,"    ");
         }
 
-        for (int day = 1; day <= daysinamonth[i]; day++)
+        for (int day = 1; day <= daysinamonth[i]; day++)//print the days in the calender
         {
             fprintf(calender,"%3d ", day);
             startday++;
@@ -63,7 +64,8 @@ void printmonth(int year)
 
         if (startday != 0) fprintf(calender,"\n");      
     }
-    fclose(calender);
+    fclose(calender);//closing the file...
+    //printing the calender in the terminal..
     for (int i = 0; i < 12; i++)
     {
         printf("\n\n-------------%s %d-------------\n", month[i], year);
@@ -95,8 +97,8 @@ int main()
     printf("Enter the year: ");
     scanf("%d", &year);
 
-    printmonth(year);
-    getchar;
+    printmonth(year);//caling printmonth to print the calender..  
 
     return 0;
+
 }
